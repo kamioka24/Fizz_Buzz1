abcbe4f1c0cd776651eb5db28a66918b3eecda8a
kamioka24 <keee2404@gmail.com>
2020-08-25 09:21:23 +0900

    [Add] test

--- test/fizz_buzz_test.rb
+++ test/fizz_buzz_test.rb
@@ -13,3 +13,21 @@ class FizzBuzzTest < Minitest::Test
 		assert_equal 'Fizz Buzz', fizz_buzz(15)
 	end
 end
+
+# Rspecの場合----------------------------------------
+
+# require './lib/fizz_buzz'
+
+# RSpec.describe 'Fizz Buzz' do
+# 	example 'fizz_buzz_' do
+# 		expect(fizz_buzz(1)).to eq '1'
+# 		expect(fizz_buzz(2)).to eq '2'
+# 		expect(fizz_buzz(3)).to eq 'Fizz'
+# 		expect(fizz_buzz(4)).to eq '4'
+# 		expect(fizz_buzz(5)).to eq 'Buzz'
+# 		expect(fizz_buzz(6)).to eq 'Fizz'
+# 		expect(fizz_buzz(15)).to eq 'Fizz Buzz'
+# 	end
+# end
+
+# -------------------------------------------------
